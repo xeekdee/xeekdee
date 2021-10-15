8f6643a67492ab0ff52a035e2cd73114274a6607
xeekdee <90238264+xeekdee@users.noreply.github.com>
2021-10-14 22:28:15 -0400

    Update lok.lua

--- lok.lua
+++ lok.lua
@@ -36,7 +36,7 @@ local tableinsert = table.insert
 -- // Silent Aim Vars
 getgenv().Aiming = {
     Enabled = true,
-    ShowFOV = true,
+    ShowFOV = false,
     FOVSides = 15,
     FOVColour = Color3fromRGB(231, 84, 128),
     VisibleCheck = true,
